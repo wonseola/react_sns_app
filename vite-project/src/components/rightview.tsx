98bc35c5375d6fd79361ac2e8936357bb89aec8d
WonSeola <39373661+wonseola@users.noreply.github.com>
2024-01-30 20:29:08 +0900

    ㅋㅋ 킹받네,, 우측에 뭐 넣을까나

--- vite-project/src/components/rightview.tsx
+++ vite-project/src/components/rightview.tsx
@@ -1,28 +1,37 @@
-
-
+import React, { useState } from "react";
 
 export default function Rightview() {
-    // const [result, setResult] = useState<string | null>(null);
+    const [result, setResult] = useState<string | null>(null);
+    const [userInput, setUserInput] = useState<string>('');
 
-    // const guessNumber = () => {
-    //     const randomNumber = Math.floor(Math.random() * 10);
-    //     const userGuess = prompt('0부터 9까지의 숫자를 입력하세요:');
+    const guessNumber = () => {
+        const randomNumber = Math.floor(Math.random() * 10);
 
-    //     if (userGuess !== null && !isNaN(Number(userGuess))) {
-    //         if (parseInt(userGuess) === randomNumber) {
-    //             setResult('정답입니다! 🎉');
-    //         } else {
-    //             setResult(`틀렸습니다. 정답은 ${randomNumber}입니다.`);
-    //         }
-    //     } else {
-    //         alert('올바른 숫자를 입력하세요.');
-    //     }
-    // };
+        if (userInput !== '' && !isNaN(Number(userInput))) {
+            if (parseInt(userInput) === randomNumber) {
+                setResult('정답입니다! 🎉');
+            } else {
+                setResult(`틀렸습니다. 정답은 ${randomNumber}입니다.`);
+            }
+        } else {
+            alert('올바른 숫자를 입력하세요.');
+        }
+    };
 
-
+    const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setUserInput(e.target.value);
+    };
 
     return (
-        // 
-        <h1></h1>
+        <div>
+            <textarea
+                value={userInput}
+                onChange={handleInputChange}
+                placeholder="0부터 9까지의 숫자를 입력하세요"
+            />
+            <br />
+            <button onClick={guessNumber}>Guess a Number</button>
+            <h1>{result}</h1>
+        </div>
     );
 }
